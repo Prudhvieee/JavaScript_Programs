ad477dac061a3059b04a79ba7290977a8a3b18df
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-12-06 08:48:52 +0530

    [Prudhvi] add . ifelse programs

--- IfElse/FlipCoin.js
+++ IfElse/FlipCoin.js
@@ -1,5 +1,4 @@
 const HEAD = 0;
 const TAIL = 1;
 let oneOrZero = (Math.random()>=0.5)? 1 : 0
-if (oneOrZero == HEAD) console.log("Heads");
- else console.log("Tails");
+oneOrZero==HEAD?console.log("Heads"):console.log("Tails");
